a3f4cb48f5c483cf492f46811302eb8b59032cb6
Richa Sharma <ris.dhiman04@gmail.com>
2026-01-20 13:12:38 +0000

    updated version

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -11,13 +11,16 @@ console.log("02" > 1);
 //few more  ...such type can cause confusions so try to avoid them
 console.log("few more");
 console.log(null > 0); //false
+console.log(null <0);
 console.log(null == 0); //false
-console.log(null >= 0); // =>true as  , here null is converted to a number zero 
+console.log(" >= & <= works differently in comparison to == ");
+console.log(null >= 0); // =>true as  , here null is converted to a number zero as >=,<= converts null to 0
  
 console.log("few more for undefined");
+//undefined with any operator guves value false either == , > ,< etc.
 console.log(undefined ==0);
 console.log(undefined >0);
 console.log(undefined<0);
- //strict checkinh
+ //strict checking
  console.log("strict check");
  console.log("2" === 2); //here  gives false bcoz in case of === it checks dtatypes too, thus here datatypes are different in this case
