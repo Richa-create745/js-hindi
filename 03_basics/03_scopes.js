2853f17b01943587492318b276339ce102f0caac
Richa Sharma <ris.dhiman04@gmail.com>
2026-03-22 15:32:16 +0000

    updates

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,30 +1,75 @@
-//about scopes
-let a= 10;
-const b=26;
-var q= 89;
+
+
+//about scopes03
+let a = 10;
+const b = 26;
+var q = 89;
 console.log(a);
 console.log(b);
 console.log(q);
 //console.log(b);
 
 //but all have different role in term of scope
- if(true) {
-    let x= 23;
-    const p= 45;
-    var q= 44;
-    //curly braces show scope ..
-    //in variables ka scope sirf in braces k andr hai...this is called "block scope"
- }
- //outside block,,,its called "global scope"
- //console.log(x); //not defined bcoz out of scope print kr rhe h
- //console.log(p); // same reason
- console.log(q); // but this output as 44,,even its out of scope.....thats the major problem with "var"
+if (true) {
+   let x = 23;
+   const p = 45;
+   var q = 44;
+   //curly braces show scope ..
+   //in variables ka scope sirf in braces k andr hai...this is called "block scope"
+}
+//outside block,,,its called "global scope"
+//console.log(x); //not defined bcoz out of scope print kr rhe h
+//console.log(p); // same reason
+console.log(q); // but this output as 44,,even its out of scope.....thats the major problem with "var"
 
 
- if(true){
-    let a =100;
-    console.log("INNER VALUE OF a in its block is :" + a)
- }
+if (true) {
+   let a = 100;
+   console.log("INNER VALUE OF a in its block is :" + a)
+}
 
- console.log(a) //still gives 10 =>global scope
-  
+console.log(a) //still gives 10 =>global scope
+
+// nested funtions =>inner function variables can access outer funtion variables but vice versa is not true
+
+function one() {
+   const username = "hitesh"
+   function two() {
+      const website = "utube"
+      console.log(username)// accessible
+
+   }
+   //console.log(website);// not accessible  =>will give error
+
+   two();
+}
+one();
+
+// if else
+if (true) {
+   const userName = "richa"
+   if (userName === "richa") {
+      const webs = "google.com"
+      console.log(webs +" "+ userName)
+   }
+   //console.log(webs); //out of scope => not accessible
+}
+//console.log(userName); // similarly not accesible
+
+// ++++++++++++++++++INTERSETING CONCEPT +++++++++++++++++++++++++
+
+
+addOne(5) ;//no error in this case if we access the function above function declaration
+function addOne(num) {
+   return num + 1;
+}
+addOne(5); // only returning not printing
+console.log(addOne(5));
+
+
+// addTwo(8) ; =>not possible in this case bocz thi s is different from the previous one
+const addTwo = function(nums){ //this function is like a expression, holding return value laong wiht function declaration.
+   return nums+2;
+}
+addTwo(8); //still only returning the value 
+console.log(addTwo(8));
---
+++ 03_basics/04_functions.js
@@ -0,0 +1,65 @@
+// about THIS and arrow function
+
+
+//creating an object
+const user = {
+    username: "suyash",
+    price: 99,
+    //a function inside an object
+    welcomeMsg: function () {
+        console.group(`${this.username} , welcome to the platform`); //this keyword is used to to access current context..jse filhal current context h object ka whole content.... so here we r accessing username
+        console.log(this); //?
+    }
+}
+user.welcomeMsg();
+
+//if we chnage the username
+user.username = "sam"; //so context is chn=anged now
+user.welcomeMsg();
+// try this line on console...it gives window
+console.log(this) //gives this => {} that is empty object
+
+
+//NOTE => BROWSER ke andr jo globale object ha uska naam h - window object
+
+// function chai(){
+//     let name = "richasharma"
+//     console.log(this)
+//      console.log(this.name) // undefined...as this context is working in objects only not in functions
+// }
+// chai()
+
+
+//other way 
+// const chai =function(){
+//     let name = " suraj"
+//     console.log(this.name) //again undefined
+// }
+// chai()
+
+
+//ARROW FUNCTIONS
+//USE => at place of function
+const chai = () => {
+    let name = "athrva"
+    console.log(this.name) //undefined
+    console.log(this) // gives e,pty object --> {}
+}
+chai()
+
+
+// purely about arrow functions ....syntax is :
+const addt = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addt(8, 3));
+
+//other way ...called implicit return
+const add = (num1, num2) => (num1 + num2) //or simply without parenthesis..... n if we enclose them in curly braces then add return  statement
+console.log(add(8, 9));
+
+
+const NAME = (n1,n2) => ({username : "aarjeev"})
+console.log(NAME(3,2)) // keep the things in curly braces to avoid undefined
+
+
